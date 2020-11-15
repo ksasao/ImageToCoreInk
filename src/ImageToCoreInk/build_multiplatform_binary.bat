f5aede121823a6016e60b00138c2952f68877cb4
ksasao <k.sasao@gmail.com>
2020-11-15 12:54:57 +0900

    Update batch

--- src/ImageToCoreInk/build_multiplatform_binary.bat
+++ src/ImageToCoreInk/build_multiplatform_binary.bat
@@ -1,6 +1,22 @@
 SET BASE_NAME=ImageToCoreInk_0.0.2_
-dotnet publish -r win10-x64 -c Release -p:PublishSingleFile=true -o ./bin/Publish/%BASE_NAME%win10-x64
-dotnet publish -r osx-x64 -c Release -p:PublishSingleFile=true -o ./bin/Publish/%BASE_NAME%osx-x64
-dotnet publish -r linux-x64 -c Release -p:PublishSingleFile=true -o ./bin/Publish/%BASE_NAME%linux-x64
-cd ./bin/Publish
+SET PUBLISH_PATH=.\bin\Publish\
+
+SET WIN_PATH=%PUBLISH_PATH%%BASE_NAME%win10-x64
+SET OSX_PATH=%PUBLISH_PATH%%BASE_NAME%%osx-x64
+SET LINUX_PATH=%PUBLISH_PATH%%BASE_NAME%%linux-x64
+
+dotnet publish -r win10-x64 -c Release -p:PublishSingleFile=true -o %WIN_PATH%
+dotnet publish -r osx-x64 -c Release -p:PublishSingleFile=true -o %OSX_PATH%
+dotnet publish -r linux-x64 -c Release -p:PublishSingleFile=true -o %LINUX_PATH%
+
+copy ..\..\LICENSE %WIN_PATH%
+copy ..\..\LICENSE %OSX_PATH%
+copy ..\..\LICENSE %LINUX_PATH%
+
+copy ..\..\README.md %WIN_PATH%
+copy ..\..\README.md %OSX_PATH%
+copy ..\..\README.md %LINUX_PATH%
+
+pushd %PUBLISH_PATH%
 del /s /q *.pdb
+popd
