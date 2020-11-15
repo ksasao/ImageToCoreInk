5f7aac54e853834136760f93bbc2ee0df100dd5e
ksasao <k.sasao@gmail.com>
2020-11-15 11:59:17 +0900

    Update multi-platform build tool.

--- src/ImageToCoreInk/build_multiplatform_binary.bat
+++ src/ImageToCoreInk/build_multiplatform_binary.bat
@@ -1,3 +1,6 @@
-dotnet publish -r win10-x64 -c Release /p:PublishSingleFile=true
-dotnet publish -r osx-x64 -c Release /p:PublishSingleFile=true
-dotnet publish -r linux-x64 -c Release /p:PublishSingleFile=true
+SET BASE_NAME=ImageToCoreInk_0.0.2_
+dotnet publish -r win10-x64 -c Release -p:PublishSingleFile=true -o ./bin/Publish/%BASE_NAME%win10-x64
+dotnet publish -r osx-x64 -c Release -p:PublishSingleFile=true -o ./bin/Publish/%BASE_NAME%osx-x64
+dotnet publish -r linux-x64 -c Release -p:PublishSingleFile=true -o ./bin/Publish/%BASE_NAME%linux-x64
+cd ./bin/Publish
+del /s /q *.pdb
